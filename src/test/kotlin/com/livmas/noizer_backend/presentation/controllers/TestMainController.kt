83d1a9ce376bbf139a4258234d46080708441201
livmas <seiranyan.sg@gmail.com>
2024-05-20 15:35:58 +0300

    Controller test created; Diagrams dir created;

--- src/test/kotlin/com/livmas/noizer_backend/presentation/controllers/TestMainController.kt
+++ src/test/kotlin/com/livmas/noizer_backend/presentation/controllers/TestMainController.kt
@@ -1,19 +1,18 @@
 package com.livmas.noizer_backend.presentation.controllers
 
 import com.livmas.noizer_backend.data.entities.TrackEntity
-import com.livmas.noizer_backend.data.services.TrackService
 import com.livmas.noizer_backend.domain.usecases.GetTracksByTitleUseCase
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 
 @SpringBootTest
@@ -34,7 +33,7 @@ class TestMainController {
 
     @Test
     fun testSearchByTitleEndpoint() {
-        every { getTracksByTitleUseCase.execute("Test") } returns tracks;
+        every { getTracksByTitleUseCase.execute("Test") } returns tracks
 
         mockMvc.perform(get("/search?title=Test"))
             .andExpect(status().isOk)
